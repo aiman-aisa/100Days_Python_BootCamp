c1cb048117862474ea000d3ab9e4e72fff942848
aiman-aisa <aiman.ab.ghapar@gmail.com>
2024-05-11 17:48:00 +0800

    move paddle done

--- Day-22 Pong Game/main.py
+++ Day-22 Pong Game/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen
+from paddle import Paddle
 
 
 screen = Screen()
@@ -6,6 +7,19 @@ screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
+
+paddle_1 = Paddle()
+
+
+screen.listen()
+screen.onkey(paddle_1.up, "Up")
+screen.onkey(paddle_1.down, "Down")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+
 
 
 
