4e5fed6d48bfb6e414e94968cd320ba5d07c870d
aiman-aisa <aiman.ab.ghapar@gmail.com>
2024-05-17 16:38:06 +0800

    error handling

--- Day-26-List-Comprehension/NATO-alphabet-start/main.py
+++ Day-26-List-Comprehension/NATO-alphabet-start/main.py
@@ -1,34 +1,40 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"], 
+#     "score": [56, 76, 98]
+# }
 
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
 
 import pandas as pd
-student_data_frame = pd.DataFrame(student_dict)
+# student_data_frame = pd.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+# {"A": "Alfa", "B": "Bravo"}
 df = pd.read_csv(r"Day-26-List-Comprehension\NATO-alphabet-start\nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (_, row) in df.iterrows()}
-#print(nato_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Please enter your input: ").upper()
+def generate_phonetic():
+    user_input = input("Please enter your input: ").upper()
 
-#result = [value for (key,value) in nato_dict.items() for letter in user_input if letter == key]
-result = [nato_dict[letter] for letter in user_input]
-print(result)
+    try:
+        result = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(result)
+        
+generate_phonetic()
--- Day-30-Error-Handling/Password-manager-enhanced/main.py
+++ Day-30-Error-Handling/Password-manager-enhanced/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
@@ -24,6 +25,12 @@ def save():
     website = website_input.get() 
     email = email_input.get()
     password = pwd_input.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
     
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Opps", message="Please don't leave any fields empty")
@@ -31,12 +38,38 @@ def save():
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
                                                     f"\nPassword: {password} \nIs it ok to save?")
         if is_ok:
-            with open(r"Day-29-Password-Manager\data.txt", "a") as password_file:
-                data = f"{website} | {email} | {password}\n"
-                password_file.write(data)
-            website_input.delete(0, END)
-            email_input.delete(0, END)
-            pwd_input.delete(0, END)
+            try:
+                with open(r"Day-30-Error-Handling\Password-manager-enhanced\data.json", "r" ) as password_file:
+                    # Reading old data
+                    data = json.load(password_file)
+            except FileNotFoundError:
+                with open(r"Day-30-Error-Handling\Password-manager-enhanced\data.json", "w" ) as password_file:
+                    json.dump(new_data, password_file, indent=4)
+            else:
+                # Update old data with new data
+                data.update(new_data)
+                
+                with open(r"Day-30-Error-Handling\Password-manager-enhanced\data.json", "w" ) as password_file:
+                    # Write the whole data
+                    json.dump(data, password_file, indent=4)
+            finally:
+                website_input.delete(0, END)
+                pwd_input.delete(0, END)
+                
+# ---------------------------- SEARCH WEBSITE DETAILS ------------------------------- #
+def search():
+    website = website_input.get()
+    try:
+        with open(r"Day-30-Error-Handling\Password-manager-enhanced\data.json", "r" ) as password_file:
+            data = json.load(password_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data.keys():
+            messagebox.showinfo(title=website, message=f"Email: {data[website]['email']}\nPassword: {data[website]['password']}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.") 
+    
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -45,7 +78,7 @@ window.config(padx=50, pady=50)
 
 
 canvas = Canvas(width=200, height=200)
-logo_img = PhotoImage(file=r"Day-29-Password-Manager\logo.png")
+logo_img = PhotoImage(file=r"Day-30-Error-Handling\Password-manager-enhanced\logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
@@ -58,21 +91,23 @@ pwd_label = Label(text="Password: ")
 pwd_label.grid(column=0, row=3)
 
 #Entries
-website_input = Entry(width=43)
-website_input.grid(column=1, row=1, columnspan=2)
+website_input = Entry(width=25)
+website_input.grid(column=1, row=1, pady=5)
 website_input.focus()
 
 email_input = Entry(width=43)
-email_input.grid(column=1, row=2, columnspan=2)
-email_input.insert(0, "fulan@gmail.com")
+email_input.grid(column=1, row=2, columnspan=2, pady=5)
+email_input.insert(0, "aiman.ab.ghapar@gmail.com")
 
 pwd_input = Entry(width=25)
-pwd_input.grid(column=1, row=3, ipadx=0)
+pwd_input.grid(column=1, row=3, ipadx=0, pady=5)
 
 #Buttons
-pwd_button = Button(text="Generate Password", width=14, padx=0, command=generate_password)
+search_button = Button(text="Search", width=14, command=search)
+search_button.grid(column=2, row=1)
+pwd_button = Button(text="Generate Password", width=14, command=generate_password)
 pwd_button.grid(column=2, row=3)
 add_button = Button(text="Add", width=36, command=save)
-add_button.grid(column=1, row=4, columnspan=2, ipadx=5)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
