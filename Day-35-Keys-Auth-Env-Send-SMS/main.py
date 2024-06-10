3995fca76c1095409c64c0ebafff7856f9e43492
aiman-aisa <aiman.ab.ghapar@gmail.com>
2024-06-10 16:30:30 +0800

    day 35 done

--- Day-35-Keys-Auth-Env-Send-SMS/main.py
+++ Day-35-Keys-Auth-Env-Send-SMS/main.py
@@ -1,13 +1,36 @@
 
-from config import api_key
+from config import api_key, twilio_sid, twilio_auth_token
 import requests
+from twilio.rest import Client
 
 parameters = {
     "lat": 2.961015,
     "lon": 101.795743,
-    "app_id": api_key
+    "cnt": 4,
+    "appid": api_key,
 }
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
-print(response)
-response.raise_for_status()
+# print(response.json())
+
+weather_data = response.json()["list"]
+# print(weather_data)
+
+will_rain = False
+
+for value in weather_data:
+    condition_code = value['weather'][0]['id']
+    if condition_code < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(twilio_sid, twilio_auth_token)
+    message = client.messages.create(
+        body = "It's going to rain today. Remember to bring an umbrella!",
+        from_ = "+17012531948",
+        to = "+60194745451"
+    )
+    print(message.status)
+    
+
+
