aae1c71e0d00d1b8da8f8a46e503d599dec4b411
aiman-aisa <aiman.ab.ghapar@gmail.com>
2024-06-15 16:10:46 +0800

    day 38 step 2

--- Day-39-Flight-Deal-Finder/data_manager.py
+++ Day-39-Flight-Deal-Finder/data_manager.py
@@ -1,3 +1,24 @@
+from pprint import pprint
+import requests
+from config import APIKEY, APISECRET, BEARER
+
+SHEET_ENDPOINT = "https://api.sheety.co/efbbf3aa2103139d2e4fb1ec3c7c4e8d/flightDeals/prices"
+
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    
+    def __init__(self):
+        self.headers = {
+            "Authorization": BEARER
+        }
+        self.destination_data = {}
+    
+    def get_destination_data(self):
+        response = requests.get(url=SHEET_ENDPOINT, headers=self.headers)
+        #print(response.text)
+        data = response.json()
+        self.destination_data = data["prices"]
+        #pprint(data)
+        
+        return self.destination_data
--- Day-39-Flight-Deal-Finder/main.py
+++ Day-39-Flight-Deal-Finder/main.py
@@ -1,1 +1,7 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+from data_manager import DataManager
+
+dataManager = DataManager()
+sheet_data = dataManager.get_destination_data()
+print(sheet_data)
